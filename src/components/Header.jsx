3d118a7830d72ca2efaae7b1053b9091d32d7e92
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2025-03-13 00:08:44 +0300

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import ToggleButton from "./ToggleButton.jsx"
 import Logo from "../assets/logo.png"
 
 function Header() {
     
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 600)
+  const [isMobile, setIsMobile] = useState(() => {
+    return window.innerWidth < 600 ? true : false
+  })
+
+  const menuRef = useRef(null)
 
   const checkScreenSize = () => {
-    setIsMobile(window.innerWidth < 600)
+    setIsMobile(() => {
+      return window.innerWidth < 600 ? true : false
+    })
   }
 
   useEffect(() => {
@@ -25,8 +31,33 @@ function Header() {
   }, [])
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen)
+    setIsDropdownOpen(prev => !prev)
   }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+        if (
+            menuRef.current &&
+            event.target instanceof Node &&
+            !menuRef.current.contains(event.target)
+        ) {
+            setIsDropdownOpen(false)
+        }
+    }
+
+    // Add event listener when the menu is open
+    if (isDropdownOpen) {
+        document.addEventListener("mousedown", handleClickOutside)
+        document.addEventListener("touchstart", handleClickOutside)
+    }
+
+    // Clean up event listeners on unmount of the component or when the menu closes
+    // to prevent memory leaks or unwanted behavior
+    return () => {
+        document.removeEventListener("mousedown", handleClickOutside)
+        document.removeEventListener("touchstart", handleClickOutside)
+    }
+}, [isDropdownOpen, menuRef])
 
   return (
     <>
@@ -37,10 +68,16 @@ function Header() {
 
       <div className="navbar">
         {isMobile ? (
-          <div className="header-mobile-menu" onClick={toggleDropdown}>
-            <i className="fa-solid fa-bars burger-icon"></i>
+          <div
+            className="header-mobile-menu">
+            <i
+              className="fa-solid fa-bars burger-icon"
+              onClick={toggleDropdown}>
+            </i>
             {isDropdownOpen && (
-              <div className="mobile-menu-opened">
+              <div
+                className={`mobile-menu-opened ${isDropdownOpen ? "slide-in" : ""}`}
+                ref={menuRef}>
                 <a href="#main">Upscaler</a>
                 <a href="#how-it-works">How It Works</a>
                 <a href="#authors">Authors</a>
