dadcde259222f537ae636d17a73e3e0725c48ece
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2025-04-02 03:25:40 +0300

    Adding ThemeContext and updating App, ToggleButton, and Main

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 // import { Helmet } from "react-helmet"
 import App from "./App"
+import { ThemeProvider } from "./context/ThemeContext"
 import "./index.css"
 
 const rootElement = document.getElementById("root")
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
+      <ThemeProvider>
         <App />
-    </React.StrictMode>,
+      </ThemeProvider>
+    </React.StrictMode>
   )
 } else {
   console.error("Element with ID 'root' not found")
